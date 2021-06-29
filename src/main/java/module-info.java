ac6c7e4b04d279d2252ebaa9180831e6dc97bf0f
AO Industries, Inc <support@aoindustries.com>
2021-06-29 02:45:05 -0500

    Now supports Java 9+ modules.

---
+++ src/main/java/module-info.java
@@ -0,0 +1,24 @@
+/*
+ * semanticcms-resources-all - Convenience POM to include most types of SemanticCMS resources, not including documentation or examples.
+ * Copyright (C) 2021  AO Industries, Inc.
+ *     support@aoindustries.com
+ *     7262 Bull Pen Cir
+ *     Mobile, AL 36695
+ *
+ * This file is part of semanticcms-resources-all.
+ *
+ * semanticcms-resources-all is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * semanticcms-resources-all is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with semanticcms-resources-all.  If not, see <http://www.gnu.org/licenses/>.
+ */
+module com.semanticcms.resources.all {
+}
