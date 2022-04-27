ed7db4cc52f59833fd00dcbcde319d79969440bc
AO Industries, Inc <support@aoindustries.com>
2022-04-27 00:05:20 -0500

    Activated org.openrewrite.java.cleanup.Cleanup

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,6 @@
 /*
  * semanticcms-resources-all - Convenience POM to include most types of SemanticCMS resources, not including documentation or examples.
- * Copyright (C) 2021  AO Industries, Inc.
+ * Copyright (C) 2021, 2022  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -21,4 +21,4 @@
  * along with semanticcms-resources-all.  If not, see <https://www.gnu.org/licenses/>.
  */
 module com.semanticcms.resources.all {
-}
+} // TODO: Avoiding rewrite-maven-plugin-4.22.2 truncation
